c3e041fa434bdee185aebd234bacb40aa9a5e703
RainKaldwin <r.sannarain@gmail.com>
2024-03-08 09:28:00 -0300

    segundo dia

---
+++ MainAuto.java
@@ -0,0 +1,23 @@
+public class MainAuto {
+    
+    public void maximaVelocidad(){
+        System.out.println("El auto esta llendo lo mas rapido posible");
+
+    }
+
+    public void velocidad(int maximaVelocidad){
+        System.out.println("La velocidad maxima es:"+maximaVelocidad);
+
+    }
+
+    public static void main(String[] args) {
+        MainAuto myAuto = new MainAuto();
+        myAuto.maximaVelocidad();
+        myAuto.velocidad(200);
+    }
+
+
+
+
+
+}
---
+++ MainConstructor.java
@@ -0,0 +1,19 @@
+public class MainConstructor {
+    int x;
+
+
+    public MainConstructor(int y)
+    {
+        x=y;
+    }
+
+    public MainConstructor(){
+        x=5;
+    }
+
+    public static void main(String[] args) {
+        MainConstructor oMain=new MainConstructor();
+        System.out.println(oMain.x);
+        System.out.println(20);
+    }
+}
