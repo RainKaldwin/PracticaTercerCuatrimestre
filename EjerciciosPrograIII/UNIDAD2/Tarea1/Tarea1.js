f90b41baf04c3d41d10be9b2a0e1ebade976ccfd
RainKaldwin <r.sannarain@gmail.com>
2024-03-12 17:17:30 -0300

    javascript

---
+++ EjerciciosPrograIII/UNIDAD2/Tarea1/Tarea1.js
@@ -0,0 +1,7 @@
+function mostrarDatos() {
+    var nombre = document.getElementById("nombreInput").value;
+    var apellido = document.getElementById("apellidoInput").value;
+
+    // Display variables
+    alert("Nombre: " + nombre + "\nApellido: " + apellido);
+}
