105936cbd0d9e4b285346918f3dc6423a90ddc26
RainKaldwin <r.sannarain@gmail.com>
2024-03-14 15:57:03 -0300

    Node.js

---
+++ TutorialYoutube/Practica Node Js/index.js
@@ -0,0 +1,9 @@
+console.log('Hello World Node.js');
+
+//podemos hacer correr el codigo
+//js en node js usando node seguido de el nombre del archivo
+//en consola
+
+/*
+1-Node Js tiene un conjunto de identificadores integrados
+*/ 
